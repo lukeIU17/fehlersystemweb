c94d3088c830279036ef256acb9689e47ee75add
atat072 <paulhenke46@gmail.com>
2024-09-04 16:47:29 +0200

    implemnted data model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,45 @@
 import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 
 const schema = a.schema({
-    Todo: a.model({
-        content: a.string(),
-        isDone: a.boolean()
+
+    Course: a.model({
+        courseID: a.id(),
+        courseName: a.string(),
+        material: a.hasMany('Material', 'courseID')
     })
-        .authorization(allow => [allow.publicApiKey()])
+        .authorization(allow => [allow.publicApiKey()]),
+
+    Material: a.model({
+        materialID: a.id(),
+        materialName: a.string(),
+        materialType: a.enum(["Script","Repetorium", "Probeklausur", "Video", "Script Fragen", "Kurs Fragen", "Literatur Hinweis"]),
+        materialDescription: a.string(),
+        courseID: a.id(),
+        course: a.belongsTo('Course', 'courseID'),
+        material: a.hasMany('Error', 'materialID'),
+    })
+        .authorization(allow => [allow.publicApiKey()]),
+
+    Error: a.model({
+        errorID: a.id(),
+        errorName: a.string(),
+        errorType: a.enum(["Rechtschreibfehler", "grammatikalischer Fehler", "inhaltlicher Fehler", "veraltete Information",
+            "nicht Verfügbarkeit von verlinkten Materialien", "verbesserungs Vorschlag"]),
+        description: a.string(),
+        status: a.enum(["Neu", "in Bearbeitung", "warten auf Feedback", "Gelöst", "Abgelehnt"]),
+        materialID: a.id(),
+        material: a.belongsTo('Material', 'materialID'),
+        error: a.hasMany('Addition', 'errorID'),
+    })
+        .authorization(allow => [allow.publicApiKey()]),
+
+    Addition: a.model({
+        description: a.string(),
+        addtionType: a.enum(["Ergänzung", "History"],),
+        errorID: a.id(),
+        error: a.belongsTo('Error', 'errorID')
+    })
+        .authorization(allow => [allow.publicApiKey()]),
 });
 
 // Used for code completion / highlighting when making requests from frontend
